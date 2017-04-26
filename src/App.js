64ce80cabd45f6d8da73c3969e2e555982955f5c
[ccym90] <[email address]>
2017-04-26 17:16:27 +0800

    record page added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
 import { Grid, Row, Col } from 'react-bootstrap';
-import Webcam from './components/webcam';
-import Playback from './components/playbackvideo';
-import Userform from './components/form';
-import Buttons from './components/buttons';
 import Header from './components/header';
-
-
+import Recordpage from './pages/recordpage';
 
 class App extends Component {
   render() {
@@ -17,28 +12,7 @@ class App extends Component {
         <div className="header">
           <Header />
         </div>
-        <Grid>
-          <Row xs={6} md={6} >
-            <Row className="videorow">
-                <Col xs={6} md={6} />
-                  <Webcam />
-                <Col xs={6} md={6} />
-                  <Playback />
-            </Row>
-            <hr/>
-            <br/>
-            <Row className='buttonrow'>
-                <Buttons />
-            </Row>
-            <br/>
-            <hr/>
-            <Row className="formrow">
-              <Userform />
-            </Row>
-            <hr/>
-            <br/>
-          </Row>
-        </Grid>
+        <Recordpage />
 
       </div>
     );
---
+++ src/pages/recordpage.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Grid, Row, Col } from 'react-bootstrap';
+import Webcam from '../components/webcam';
+import Playback from '../components/playbackvideo';
+import Userform from '../components/form';
+import Buttons from '../components/buttons';
+
+
+class Recordpage extends Component {
+  render() {
+    return (
+      <div className="recordpage">
+        <Grid>
+          <Row xs={6} md={6} >
+            <Row className="videorow">
+                <Col xs={6} md={6} />
+                  <Webcam />
+                <Col xs={6} md={6} />
+                  <Playback />
+            </Row>
+            <hr/>
+            <br/>
+            <Row className='buttonrow'>
+                <Buttons />
+            </Row>
+            <br/>
+            <hr/>
+            <Row className="formrow">
+              <Userform />
+            </Row>
+            <hr/>
+            <br/>
+          </Row>
+        </Grid>
+
+      </div>
+    );
+  }
+}
+
+export default Recordpage;
