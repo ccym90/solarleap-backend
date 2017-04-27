be8d9d5b106712d373bfe6ff2665dcebfbe91276
Viviane Wu <vivianewu92@gmail.com>
2017-04-28 02:04:00 +0800

    working on ajax calls

--- src/components/form.js
+++ src/components/form.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { FormGroup, Form, Button, ControlLabel, FormControl, Col } from 'react-bootstrap';
-import Tooltip from 'react-tooltip-component';
+// import { FormGroup, Form, Button, ControlLabel, FormControl, Col } from 'react-bootstrap';
+// import Tooltip from 'react-tooltip-component';
 import { connect } from 'react-redux';
 import { saving } from '../redux/actions';
+import axios from 'axios';
 
 class Userform extends React.Component {
 
@@ -13,55 +14,70 @@ class Userform extends React.Component {
     dispatch(saving());
   }
 
+  handleSubmit = (e) => {
+
+    e.preventDefault();
+
+    let data = {}
+     data.author = this.refs.authorInput.value;
+     data.title = this.refs.titleInput.value;
+     data.description = this.refs.descriptionInput.value;
+     data.topics = this.refs.topicsInput.value;
+
+    console.log('from Userform', data);
+
+    axios.post('/upload', {
+      data
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  }
+
+
+
+
   render() {
     return (
-      <Form horizontal>
-        <FormGroup controlId="formHorizontalAuthor">
-          <Col componentClass={ControlLabel} sm={2}>
-          Author Name
-          </Col>
-          <Col sm={10}>
-          <FormControl type="Author Name" placeholder="Name" />
-          </Col>
-        </FormGroup>
 
-        <FormGroup controlId="formHorizontalTitle">
-          <Col componentClass={ControlLabel} sm={2}>
-          Title
-          </Col>
-          <Col sm={10}>
-          <FormControl type="Title" placeholder="Title e.g. Addition & Subtraction" />
-          </Col>
-        </FormGroup>
+      <form onSubmit={this.handleSubmit} className="form-horizontal">
+              <div className="form-group">
+                <label htmlFor="inputTitle" className="col-sm-2 control-label">Title</label>
+                <div className="col-sm-10">
+                  <input ref="titleInput" type="text" className="form-control" id="inputPassword3" placeholder="Title" />
+                </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="inputAuthor3" className="col-sm-2 control-label">Author</label>
+                <div className="col-sm-10">
+                  <input ref="authorInput" type="text" className="form-control" id="author" placeholder="Author Name" />
+                </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="inputDescription" className="col-sm-2 control-label">Description</label>
+                <div className="col-sm-10">
+                  <input ref="descriptionInput" type="text" className="form-control" id="inputDescription3" placeholder="Description" />
+                </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="inputTopics" className="col-sm-2 control-label">Topics</label>
+                <div className="col-sm-10">
+                  <input ref="topicsInput" type="text" className="form-control" id="inputSubject3" placeholder="Topics" />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="col-sm-offset-2 col-sm-10">
+                  <button type="submit" className="btn btn-default">Save</button>
+                </div>
+              </div>
+      </form>
 
-        <FormGroup controlId="formHorizontalSubject">
-          <Col componentClass={ControlLabel} sm={2}>
-          Subject
-          </Col>
-          <Col sm={10}>
-          <FormControl type="Subject" placeholder="Subject e.g. Math" />
-          </Col>
-        </FormGroup>
 
-        <FormGroup controlId="formHorizontalDescription">
-          <Col componentClass={ControlLabel} sm={2}>
-          Video Description
-          </Col>
-          <Col sm={10}>
-          <FormControl type="Description" placeholder="Description e.g. How to add and subtract with positive and negative numbers" />
-          </Col>
-        </FormGroup>
 
-        <FormGroup>
-          <Col smOffset={7} sm={8}>
-            <Tooltip title='Click here to save your video to the library' position='right'>
-            <Button className='btn btn-primary' ref='save' type="submit" onClick={this.saveVideo}>
-            Save
-            </Button>
-            </Tooltip>
-          </Col>
-        </FormGroup>
-      </Form>
 
     )
   }
