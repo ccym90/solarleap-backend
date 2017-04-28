a4f7328c332a6f0956a3a36dbef1c3b8f21deafb
[ccym90] <[email address]>
2017-04-28 11:12:38 +0800

    thumbnail added

--- src/pages/librarypage.js
+++ src/pages/librarypage.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-import { Grid } from 'react-bootstrap';
+import { Grid, Row, Col, Button, Thumbnail } from 'react-bootstrap';
 import Header from '../components/header';
 
 class Librarypage extends Component {
   render() {
     return (
+      <div className="container">
       <div className="librarypage">
         <Header />
         <Grid>
-        <p> hello from Our library page </p>
+        <Row className="videorow">
+          <h1>Video Library</h1>
+          <hr/>
+          <p> Type in the search bar to find a video from the library. </p>
+          <Grid>
+            <Row className="tumbnails">
+            <Col xs={6} md={4}>
+              <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+                <h3>Thumbnail label</h3>
+                <p>Description</p>
+                <p>
+                  <Button bsStyle="primary">Button</Button>&nbsp;
+                  <Button bsStyle="default">Button</Button>
+                </p>
+              </Thumbnail>
+            </Col>
+            </Row>
+          </Grid>
+        </Row>
         </Grid>
+      </div>
       </div>
     );
   }
--- src/pages/uploadPage.js
+++ src/pages/uploadPage.js
@@ -13,67 +13,66 @@ class uploadPage extends Component {
       <div className="Container">
       <div className="uploadpage">
         <Header />
-        <br/>
-        <Grid>
-          <Row>
-            <Row className="formrow">
-            <h1>Upload Page</h1>
-            <hr/>
-            <p>Here you can upload a video from your mobile, or computer and save to the video library.
-            <strong> Before saving, please ensure you fill out the form below.</strong> </p>
-            <p>Information to see how to use the uploader <a href="http://w3c.github.io/mediacapture-record/MediaRecorder.html" title="W3C MediaStream Recording API Editor's Draft">Editor's&nbsp;Draft</a>.</p>
+          <br/>
+          <Grid>
+            <Row>
+              <Row className="formrow">
+                <h1>Upload Page</h1>
+                <hr/>
+                <p>Here you can upload a video from your mobile, or computer and save to the video library.
+                <strong> Before saving, please ensure you fill out the form below.</strong> </p>
+                <p>Information to see how to use the uploader <a href="http://w3c.github.io/mediacapture-record/MediaRecorder.html" title="W3C MediaStream Recording API Editor's Draft">Editor's&nbsp;Draft</a>.</p>
 
 
-            <form onSubmit={this.handleSubmit} className="form-horizontal">
-              <div className="form-group">
-                <label htmlFor="inputTitle" className="col-sm-2 control-label">Title</label>
-                <div className="col-sm-10">
-                  <input ref="titleInput" type="text" className="form-control" id="title" placeholder="e.g. Addition & Subtraction" />
+              <form onSubmit={this.handleSubmit} className="form-horizontal">
+                <div className="form-group">
+                  <label htmlFor="inputTitle" className="col-sm-2 control-label">Title</label>
+                  <div className="col-sm-10">
+                    <input ref="titleInput" type="text" className="form-control" id="title" placeholder="e.g. Addition & Subtraction" />
+                  </div>
                 </div>
-              </div>
 
-              <div className="form-group">
-                <label htmlFor="inputAuthor3" className="col-sm-2 control-label">Name</label>
-                <div className="col-sm-10">
-                  <input ref="authorInput" type="text" className="form-control" id="author" placeholder="e.g. Joe Bloggs" />
+                <div className="form-group">
+                  <label htmlFor="inputAuthor3" className="col-sm-2 control-label">Name</label>
+                  <div className="col-sm-10">
+                    <input ref="authorInput" type="text" className="form-control" id="author" placeholder="e.g. Joe Bloggs" />
+                  </div>
                 </div>
-              </div>
 
-              <div className="form-group">
-                <label htmlFor="inputDescription" className="col-sm-2 control-label">Subject</label>
-                <div className="col-sm-10">
-                  <input ref="descriptionInput" type="text" className="form-control" id="Subject" placeholder="e.g. Math" />
+                <div className="form-group">
+                  <label htmlFor="inputDescription" className="col-sm-2 control-label">Subject</label>
+                  <div className="col-sm-10">
+                    <input ref="descriptionInput" type="text" className="form-control" id="Subject" placeholder="e.g. Math" />
+                  </div>
                 </div>
-              </div>
 
-              <div className="form-group">
-                <label htmlFor="inputTopics" className="col-sm-2 control-label">Video Description</label>
-                <div className="col-sm-10">
-                  <input ref="topicsInput" type="text" className="form-control" id="description" placeholder="e.g. How to add and subtract with positive and negative numbers" />
+                <div className="form-group">
+                  <label htmlFor="inputTopics" className="col-sm-2 control-label">Video Description</label>
+                  <div className="col-sm-10">
+                    <input ref="topicsInput" type="text" className="form-control" id="description" placeholder="e.g. How to add and subtract with positive and negative numbers" />
+                  </div>
                 </div>
-              </div>
 
-              <FormGroup controlId="formHorizontalDescription">
-                <Col componentClass={ControlLabel} sm={2}>
-                Choose A File
-                <Col sm={10}>
-                <label htmlFor="FileBox"> </label><input type="file" id="FileBox" /><br />
-                </Col>
-                </Col>
-              </FormGroup>
+                <FormGroup controlId="formHorizontalDescription">
+                  <Col componentClass={ControlLabel} sm={2}>
+                  Choose A File
+                  <Col sm={10}>
+                  <label htmlFor="FileBox"> </label><input type="file" id="FileBox" /><br />
+                  </Col>
+                  </Col>
+                </FormGroup>
 
-              <div className="form-group">
-                <div className="col-sm-offset-2 col-sm-10">
-                  <button type="submit" className="btn btn-primary">Save</button>
+                <div className="form-group">
+                  <div className="col-sm-offset-2 col-sm-10">
+                    <button type="submit" className="btn btn-primary">Save</button>
+                  </div>
                 </div>
-              </div>
-            </form>
-
+              </form>
+              </Row>
+              <hr/>
+              <br/>
             </Row>
-            <hr/>
-            <br/>
-          </Row>
-        </Grid>
+          </Grid>
       </div>
       </div>
 
