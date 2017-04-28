3813b9b8099e1c8aebee95351901bd6e66b84fcc
Viviane Wu <vivianewu92@gmail.com>
2017-04-28 11:11:41 +0800

    ajax post added

--- src/pages/uploadPage.js
+++ src/pages/uploadPage.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
-import { FormGroup, Form, Button, ControlLabel, FormControl } from 'react-bootstrap';
-import Tooltip from 'react-tooltip-component';
+import { FormGroup, ControlLabel } from 'react-bootstrap';
+// import Tooltip from 'react-tooltip-component';
 import Header from '../components/header';
 import './uploadPage.css';
+import axios from 'axios';
 
 class uploadPage extends Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let data = {}
+     data.author = this.refs.authorInput.value;
+     data.title = this.refs.titleInput.value;
+     data.description = this.refs.descriptionInput.value;
+     data.topics = this.refs.topicsInput.value;
+
+
+    axios.post('/upload',
+      { title : data.title, author: data.author, description: data.description, topics: data.topics  }
+    )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  }
 
   render() {
     return (
@@ -42,14 +65,14 @@ class uploadPage extends Component {
               <div className="form-group">
                 <label htmlFor="inputDescription" className="col-sm-2 control-label">Subject</label>
                 <div className="col-sm-10">
-                  <input ref="descriptionInput" type="text" className="form-control" id="Subject" placeholder="e.g. Math" />
+                  <input ref="topicsInput" type="text" className="form-control" id="Subject" placeholder="e.g. Math" />
                 </div>
               </div>
 
               <div className="form-group">
                 <label htmlFor="inputTopics" className="col-sm-2 control-label">Video Description</label>
                 <div className="col-sm-10">
-                  <input ref="topicsInput" type="text" className="form-control" id="description" placeholder="e.g. How to add and subtract with positive and negative numbers" />
+                  <input ref="descriptionInput"  type="text" className="form-control" id="description" placeholder="e.g. How to add and subtract with positive and negative numbers" />
                 </div>
               </div>
 
