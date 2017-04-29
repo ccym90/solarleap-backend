9bdc1fb85e1152f5a750c987c42f2d72ca7d37d9
Viviane Wu <vivianewu92@gmail.com>
2017-04-29 17:31:42 +0800

    working on uploading files

--- src/pages/uploadPage.js
+++ src/pages/uploadPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
-import { FormGroup, ControlLabel } from 'react-bootstrap';
+import { Grid, Row } from 'react-bootstrap';
+// import { FormGroup, ControlLabel } from 'react-bootstrap';
 // import Tooltip from 'react-tooltip-component';
 import Header from '../components/header';
 import './uploadPage.css';
 import axios from 'axios';
+
+
 
 class uploadPage extends Component {
 
@@ -12,24 +14,32 @@ class uploadPage extends Component {
     e.preventDefault();
 
     let metaData = {}
-    metaData.author = this.refs.authorInput.value;
-    metaData.title = this.refs.titleInput.value;
-    metaData.description = this.refs.descriptionInput.value;
-    metaData.topics = this.refs.topicsInput.value;
+      metaData.author = this.refs.authorInput.value;
+      metaData.title = this.refs.titleInput.value;
+      metaData.description = this.refs.descriptionInput.value;
+      metaData.topics = this.refs.topicsInput.value;
 
     console.log(metaData)
 
+    let video = this.refs.video.value;
+    console.log(video)
+
     let file = new FormData()
-      file.append('file', document);
-      file.append('info', { title : metaData.title, author: metaData.author, description: metaData.description, topics: metaData.topics  })
+    // let form = document.forms.namedItem("videoInfo");
+    // console.log('from upload page this is form', form)
 
-      console.log(file)
+      // file.append('file', document);
+      file.append('fileInfo', metaData )
+      file.append('file', video )
 
-      // { title : data.title, author: data.author, description: data.description, topics: data.topics  }
+      console.log(document)
+
+      // { title : metaData.title, author: metaData.author, description: metaData.description, topics: metaData.topics  }
 
     axios.post('/upload', file)
       .then(function (response) {
-        console.log(response);
+        console.log(response.data);
+        console.log(video)
       })
       .catch(function (error) {
         console.log(error);
@@ -48,6 +58,8 @@ class uploadPage extends Component {
 //     </div>
 //   </div>
 
+
+
   render() {
     return (
 
@@ -64,50 +76,44 @@ class uploadPage extends Component {
                 <p>Information to see how to use the uploader <a href="http://w3c.github.io/mediacapture-record/MediaRecorder.html" title="W3C MediaStream Recording API Editor's Draft">Editor's&nbsp;Draft</a>.</p>
 
 
-              <form encType="multipart/form-data"  onSubmit={this.handleSubmit} className="form-horizontal">
+                <form encType="multipart/form-data" method="POST" name="videoInfo" onSubmit={this.handleSubmit} className="form-horizontal">
+                  <div className="form-group">
+                    <label htmlFor="inputTitle" className="col-sm-2 control-label">Title</label>
+                    <div className="col-sm-10">
+                      <input ref="titleInput" type="text" className="form-control" id="title" placeholder="e.g. Addition & Subtraction" />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="inputAuthor3" className="col-sm-2 control-label">Name</label>
+                    <div className="col-sm-10">
+                      <input ref="authorInput" type="text" className="form-control" id="author" placeholder="e.g. Joe Bloggs" />
+                    </div>
+                  </div>
                 <div className="form-group">
-                  <label htmlFor="inputTitle" className="col-sm-2 control-label">Title</label>
+                  <label htmlFor="inputDescription" className="col-sm-2 control-label">Subject</label>
                   <div className="col-sm-10">
-                    <input ref="titleInput" type="text" className="form-control" id="title" placeholder="e.g. Addition & Subtraction" />
+                    <input ref="topicsInput" type="text" className="form-control" id="Subject" placeholder="e.g. Math" />
                   </div>
                 </div>
-
                 <div className="form-group">
-                  <label htmlFor="inputAuthor3" className="col-sm-2 control-label">Name</label>
+                  <label htmlFor="inputTopics" className="col-sm-2 control-label">Video Description</label>
                   <div className="col-sm-10">
-                    <input ref="authorInput" type="text" className="form-control" id="author" placeholder="e.g. Joe Bloggs" />
+                    <input ref="descriptionInput"  type="text" className="form-control" id="description" placeholder="e.g. How to add and subtract with positive and negative numbers" />
                   </div>
                 </div>
-
-              <div className="form-group">
-                <label htmlFor="inputDescription" className="col-sm-2 control-label">Subject</label>
-                <div className="col-sm-10">
-                  <input ref="topicsInput" type="text" className="form-control" id="Subject" placeholder="e.g. Math" />
-                </div>
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="inputTopics" className="col-sm-2 control-label">Video Description</label>
-                <div className="col-sm-10">
-                  <input ref="descriptionInput"  type="text" className="form-control" id="description" placeholder="e.g. How to add and subtract with positive and negative numbers" />
-                </div>
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="inputTopics" className="col-sm-2 control-label">
-                  Choose A File</label>
-                <div className="col-sm-10">
-                    <label htmlFor="FileBox"> </label><input name='file' type="file" id="FileBox" /><br />
-                </div>
-              </div>
-
                 <div className="form-group">
-                  <div className="col-sm-offset-2 col-sm-10">
-                    <button type="submit" className="btn btn-primary">Upload</button>
+                  <label htmlFor="inputTopics" className="col-sm-2 control-label">
+                    Choose A File</label>
+                  <div className="col-sm-10">
+                      <label htmlFor="FileBox"> </label><input ref='video' name='videoFile' type="file" id="FileBox" /><br />
                   </div>
                 </div>
-
-              </form>
+                  <div className="form-group">
+                    <div className="col-sm-offset-2 col-sm-10">
+                      <button type="submit" className="btn btn-primary">Upload</button>
+                    </div>
+                  </div>
+                </form>
 
               </Row>
               <hr/>
